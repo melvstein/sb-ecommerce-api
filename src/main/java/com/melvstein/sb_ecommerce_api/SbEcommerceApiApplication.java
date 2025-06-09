70bc4ff66d8e332e89617d702c3bb315ccb8f443
melvstein <melvinbayogo@gmail.com>
2025-06-10 05:39:07 +0800

    init ecommerce api

---
+++ src/main/java/com/melvstein/sb_ecommerce_api/SbEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.melvstein.sb_ecommerce_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEcommerceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melvstein/sb_ecommerce_api/SbEcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melvstein.sb_ecommerce_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
